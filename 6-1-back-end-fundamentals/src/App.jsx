fd3b594b9e400ab02885eacf0a2c6fb6d41832fb
jawwad183 <mjwdmufti@gmail.com>
2026-03-29 20:43:19 +0300

    Auto grading implementation v4

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -360,7 +360,6 @@ export default function App() {
   // Create state to store student data
   // Syntax hint:
   // const [student, setStudent] = useState(____);
-  const [student, setStudent] = useState(null);
 
   // TODO 8:
   // Request student data from the server when the page loads
@@ -370,13 +369,6 @@ export default function App() {
   //   .then((data) => {
   //     setStudent(data);
   //   });
-  useEffect(() => {
-    fetch('http://localhost:3000/student')
-      .then((res) => res.json())
-      .then((data) => {
-        setStudent(data);
-      });
-  }, []);
 
   return (
     <main className="app-shell">
